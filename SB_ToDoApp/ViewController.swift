cea792bc99d4d5d9ad2a3d3c9e35e6764743b888
Maksim Romanov <ios38ru@gmail.com>
2021-08-12 19:14:50 +0800

    Initial Commit

---
+++ SB_ToDoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SB_ToDoApp
+//
+//  Created by Maksim Romanov on 12.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
